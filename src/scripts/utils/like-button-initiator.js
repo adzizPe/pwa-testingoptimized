a1d98817777035a76471be1920e3d8c6c2f468cf
adzizPe <adzanaziz2@gmail.com>
2024-12-01 18:40:16 +0700

    +

--- src/scripts/utils/like-button-initiator.js
+++ src/scripts/utils/like-button-initiator.js
@@ -1,36 +1,60 @@
 import FavIdb from '../data/database-idb';
 
-const LikeButtonInitiator = {
-  async init({ likeButtonContainer, data }) {
+// like-button-initiator.js
+
+class LikeButtonInitiator {
+  constructor({ likeButtonContainer, article }) {
     this._likeButtonContainer = likeButtonContainer;
-    this._data = data;
+    this._article = article;
 
-    await this._renderButton();
-  },
+    this._renderLike = this._renderLike.bind(this);
+    this._renderLiked = this._renderLiked.bind(this);
+  }
 
-  async _renderButton() {
-    const { id } = this._data;
-    console.log(this._data);
+  // Fungsi untuk render tombol "Like"
+  _renderLike() {
+    this._likeButtonContainer.innerHTML = `
+      <button aria-label="like this article" class="like-button">
+        ❤️ Like
+      </button>
+    `;
+    const likeButton = this._likeButtonContainer.querySelector('.like-button');
+    likeButton.addEventListener('click', () => this._handleLikeClick());
+  }
 
-    if (await this._isDataExist(id)) {
+  // Fungsi untuk render tombol "Liked" ketika artikel sudah di-like
+  _renderLiked() {
+    this._likeButtonContainer.innerHTML = `
+      <button aria-label="unlike this article" class="liked-button">
+        💔 Unlike
+      </button>
+    `;
+    const likedButton = this._likeButtonContainer.querySelector('.liked-button');
+    likedButton.addEventListener('click', () => this._handleUnlikeClick());
+  }
+
+  // Fungsi untuk menangani klik pada tombol "Like"
+  _handleLikeClick() {
+    // Simpan status like (misalnya ke localStorage atau API)
+    this._article.isLiked = true; 
+    this._renderLiked();
+  }
+
+  // Fungsi untuk menangani klik pada tombol "Unlike"
+  _handleUnlikeClick() {
+    // Hapus status like
+    this._article.isLiked = false; 
+    this._renderLike();
+  }
+
+  // Inisialisasi dan render tombol like berdasarkan status article
+  init() {
+    if (this._article.isLiked) {
       this._renderLiked();
     } else {
       this._renderLike();
     }
-  },
-
-  async _isDataExist(id) {
-    const resto = await FavIdb.getFavorite(id);
-    return !!resto;
-  },
-
-  async _addToFavorite(id) {
-    await FavIdb.putFavorite(id);
-  },
-
-  async _deleteFromFavorite(id) {
-    await FavIdb.deleteFavorite(id);
-  },
-};
+  }
+}
 
 export default LikeButtonInitiator;
