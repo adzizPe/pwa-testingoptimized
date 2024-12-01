3dab81ebed23407860d877b411ed1c0895676adb
adzizPe <adzanaziz2@gmail.com>
2024-12-01 20:31:26 +0700

    +

--- src/scripts/utils/like-button-initiator.js
+++ src/scripts/utils/like-button-initiator.js
@@ -1,60 +1,64 @@
 import FavIdb from '../data/database-idb';
 
-// like-button-initiator.js
-
-class LikeButtonInitiator {
-  constructor({ likeButtonContainer, article }) {
+const LikeButtonInitiator = {
+  async init({ likeButtonContainer, data }) {
     this._likeButtonContainer = likeButtonContainer;
-    this._article = article;
+    this._data = data;
 
-    this._renderLike = this._renderLike.bind(this);
-    this._renderLiked = this._renderLiked.bind(this);
-  }
+    await this._renderButton();
+  },
 
-  // Fungsi untuk render tombol "Like"
-  _renderLike() {
-    this._likeButtonContainer.innerHTML = `
-      <button aria-label="like this article" class="like-button">
-        ❤️ Like
-      </button>
-    `;
-    const likeButton = this._likeButtonContainer.querySelector('.like-button');
-    likeButton.addEventListener('click', () => this._handleLikeClick());
-  }
+  async _renderButton() {
+    const { id } = this._data;
+    console.log(this._data);
 
-  // Fungsi untuk render tombol "Liked" ketika artikel sudah di-like
-  _renderLiked() {
-    this._likeButtonContainer.innerHTML = `
-      <button aria-label="unlike this article" class="liked-button">
-        💔 Unlike
-      </button>
-    `;
-    const likedButton = this._likeButtonContainer.querySelector('.liked-button');
-    likedButton.addEventListener('click', () => this._handleUnlikeClick());
-  }
-
-  // Fungsi untuk menangani klik pada tombol "Like"
-  _handleLikeClick() {
-    // Simpan status like (misalnya ke localStorage atau API)
-    this._article.isLiked = true; 
-    this._renderLiked();
-  }
-
-  // Fungsi untuk menangani klik pada tombol "Unlike"
-  _handleUnlikeClick() {
-    // Hapus status like
-    this._article.isLiked = false; 
-    this._renderLike();
-  }
-
-  // Inisialisasi dan render tombol like berdasarkan status article
-  init() {
-    if (this._article.isLiked) {
+    if (await this._isDataExist(id)) {
       this._renderLiked();
     } else {
       this._renderLike();
     }
-  }
-}
+  },
+
+  async _isDataExist(id) {
+    const resto = await FavIdb.getFavorite(id);
+    return !!resto;
+  },
+
+  async _addToFavorite(id) {
+    await FavIdb.putFavorite(id);
+  },
+
+  async _deleteFromFavorite(id) {
+    await FavIdb.deleteFavorite(id);
+  },
+
+  _renderLike() {
+    // Render the "like" button HTML structure
+    this._likeButtonContainer.innerHTML = `
+      <button aria-label="like this restaurant" id="likeButton" class="like">
+        <i class="fa fa-heart-o" aria-hidden="true"></i>
+      </button>
+    `;
+    const likeButton = document.querySelector('#likeButton');
+    likeButton.addEventListener('click', async () => {
+      await this._addToFavorite(this._data.id);
+      this._renderLiked();
+    });
+  },
+
+  _renderLiked() {
+    // Render the "liked" button HTML structure
+    this._likeButtonContainer.innerHTML = `
+      <button aria-label="unlike this restaurant" id="likeButton" class="liked">
+        <i class="fa fa-heart" aria-hidden="true"></i>
+      </button>
+    `;
+    const likeButton = document.querySelector('#likeButton');
+    likeButton.addEventListener('click', async () => {
+      await this._deleteFromFavorite(this._data.id);
+      this._renderLike();
+    });
+  },
+};
 
 export default LikeButtonInitiator;
